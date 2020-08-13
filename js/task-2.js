7a734e62113b8231be1645845673b2efa147dbb4
Eugene1401 <eugene220117@gmail.com>
2020-08-13 14:03:21 +0300

    first

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function (itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction.call(inventory, itemName);
+};
+
+invokeInventoryOperation("Medkit", inventory.add);
+
+console.log(inventory.items);
+
+invokeInventoryOperation("Gas mask", inventory.remove);
+
+console.log(inventory.items);
